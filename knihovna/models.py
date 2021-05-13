7d3299b27fda881bc97c656079c57e02f7a9c428
Adam Rychtář <rychtar.adamek@seznam.cz>
2021-05-13 13:51:48 +0200

    Vytvoření modelu Book

--- knihovna/models.py
+++ knihovna/models.py
@@ -12,3 +12,22 @@ class Genre(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class Book(models.Model):
+    name = models.CharField(max_length=150, verbose_name="Název knihy", help_text='Zadejte název knihy.')
+    genres = models.ManyToManyField(Genre, help_text="Vyberte žánr knihy.")
+    date = models.DateField(verbose_name="Datum vydání", help_text="Zadejte datum vydání knihy.")
+    pages = models.IntegerField(blank=True, null=True, verbose_name="Počet stran", help_text="Zadejte počet stran.")
+    rate = models.FloatField(default=5.0, validators=[MinValueValidator(1.0), MaxValueValidator(10.0)], null=True,
+                             help_text="Zadejte hodnocení od 1.0 - 10.0", verbose_name="Hodnocení")
+    isbn = models.CharField(max_length=17, verbose_name="ISBN", help_text="Zadejte ISBN kód knihy.")
+
+    class Meta:
+        ordering = ["date", "name"]
+
+    def __str__(self):
+        return f"{self.name}, year: {str(self.date.year)}, rate: {str(self.rate)}"
+
+    def get_absolute_url(self):
+        return reverse('kniha-detail', args=[str(self.id)])
