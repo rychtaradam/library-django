c9aff2f43d8376f7949c771498bddbee1667deb0
Adam Rychtář <rychtar.adamek@seznam.cz>
2021-05-13 14:25:59 +0200

    Oprava předchozího úkolu do Djanga.

--- knihovna/models.py
+++ knihovna/models.py
@@ -14,9 +14,20 @@ class Genre(models.Model):
         return self.name
 
 
+class Author(models.Model):
+    name = models.CharField(max_length=100, unique=True, verbose_name="Jmeno autora", help_text="Zadejte jméno autora.")
+
+    class Meta:
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
 class Book(models.Model):
     name = models.CharField(max_length=150, verbose_name="Název knihy", help_text='Zadejte název knihy.')
-    genres = models.ManyToManyField(Genre, help_text="Vyberte žánr knihy.")
+    genre = models.ManyToManyField(Genre, help_text="Vyberte žánr knihy.")
+    author = models.ManyToManyField(Author, help_text="Vyberte autora knihy.")
     date = models.DateField(verbose_name="Datum vydání", help_text="Zadejte datum vydání knihy.")
     pages = models.IntegerField(blank=True, null=True, verbose_name="Počet stran", help_text="Zadejte počet stran.")
     rate = models.FloatField(default=5.0, validators=[MinValueValidator(1.0), MaxValueValidator(10.0)], null=True,
@@ -27,7 +38,7 @@ class Book(models.Model):
         ordering = ["date", "name"]
 
     def __str__(self):
-        return f"{self.name}, year: {str(self.date.year)}, rate: {str(self.rate)}"
+        return f"{self.name}, Rok vydání: {str(self.date.year)}, Hodnocení: {str(self.rate)}"
 
     def get_absolute_url(self):
         return reverse('kniha-detail', args=[str(self.id)])
