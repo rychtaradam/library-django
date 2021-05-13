a8670cfde337c42372bbd2c7d69eb3063980ee20
Adam Rychtář <rychtar.adamek@seznam.cz>
2021-05-13 17:48:25 +0200

    Založení templates

--- knihovna/views.py
+++ knihovna/views.py
@@ -1,3 +1,13 @@
 from django.shortcuts import render
+from knihovna.models import Book, Genre, Author
 
-# Create your views here.
+def index(request):
+    num_books = Book.objects.all().count()
+    books = Book.objects.order_by('-rate')[:3]
+
+    context = {
+        'num_books' : num_books,
+        'books' : books
+    }
+
+    return render(request, 'index.html', context=context)
